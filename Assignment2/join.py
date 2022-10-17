3c8e3ab522333bd35a8aa9c81974d4ea2d8ee098
Kar Kei Ong <candyokk@outlook.com>
2022-10-17 21:17:34 +0800

    i

--- Assignment2/join.py
+++ Assignment2/join.py
@@ -1,19 +1,18 @@
-# import pandas as pd
-
-# file1 = pd.read_csv('pm2_5.csv')
-# file2 = pd.read_csv('death-rates-from-air-pollution.csv')
-
-# print(file1)
-# file3 = pd.merge(file1, file2, on='Code')
-
-# file3 = file3[['Entity', 'Code', 'Level', 'Deaths']]
-# file3.to_csv('air_pollutions_deaths.csv')
-
-import numpy as np
 import pandas as pd
-import plotly.express as px
-import os
-from datetime import datetime
+
+# file1 = pd.read_csv('./data/temperature_changes.csv')
+# file2 = pd.read_csv('./data/deaths.csv')
+# file1 = file1[file1['Year']==2020]
+# file3 = pd.merge(file1, file2, on='ISO3 Code')
+# print(file3)
+# file3 = file3[['Country_y', 'ISO3 Code', 'Value', 'Deaths']]
+# file3.to_csv('temperature_deaths.csv')
+
+# import numpy as np
+# import pandas as pd
+# import plotly.express as px
+# import os
+# from datetime import datetime
 
 # data = []
 # data.append(['Iceberg', 'Length', 'Width', 'Latitude', 'Longitude', 'Remarks', 'Last_update'])
@@ -61,6 +60,54 @@ from datetime import datetime
 # df = pd.read_csv('country_temperature2.csv')
 # df = df.dropna()
 # df.to_csv('country_temperature2.csv')
-df = pd.read_csv('./data/FAOSTAT_data_1-10-2022.csv')
-df = df[df['Months']=='December']
-df.to_csv('temperature_changes.csv')
+# df = pd.read_csv('./data/FAOSTAT_data_1-10-2022.csv')
+# df = df[df['Months']=='December']
+# df2 = pd.read_csv('./data/FAOSTAT_data_11-24-2020.csv')
+# df3 = pd.merge(df, df2, on='Country')
+# # print(df3)
+# df3.to_csv('temperature_changes.csv')
+
+# df = pd.read_csv('./data/global-fossil-fuel-consumption.csv')
+# gas = df[['Year', 'Gas (TWh)']]
+# oil = df[['Year', 'Oild (TWh)']]
+# coal = df[['Year', 'Coal (TWh)']]
+# gas = gas.rename(columns={'Gas (TWh)': 'Value'})
+# oil = oil.rename(columns={'Oild (TWh)': 'Value'})
+# coal = coal.rename(columns={'Coal (TWh)': 'Value'})
+# gas['type'] = 'Gas (TWh)'
+# oil['type'] =  'Oil (TWh)'
+# coal['type'] = 'Coal (TWh)'
+
+df = pd.read_csv('./data/ice_age.csv')
+one = df[['Year', '1YI']]
+two = df[['Year', '2YI']]
+three = df[['Year', '3YI']]
+four = df[['Year', '4YI']]
+five = df[['Year', '5+YI']]
+
+one = one.rename(columns={'1YI': 'Value'})
+two = two.rename(columns={'2YI': 'Value'})
+three = three.rename(columns={'3YI': 'Value'})
+four = four.rename(columns={'4YI': 'Value'})
+five = five.rename(columns={'5+YI': 'Value'})
+
+one['type'] = 1
+two['type'] = 2
+three['type'] = 3
+four['type'] = 4
+five['type'] = 5
+
+df2 = pd.concat([one, two, three, four, five], axis=0)
+df2.to_csv('seaice_age.csv')
+
+# df2 = pd.concat([gas, oil, coal], axis=0)
+# # df3 = pd.merge(df2, coal, on='Year')
+# print(df2)
+# df2.to_csv('fossils_fuel_usage.csv')
+# df = pd.read_csv('./data/seaice.csv')
+# df = df[df['hemisphere'] == 'north']
+# df = df[['Year', 'Month', 'Extent']]
+# df2 = df.groupby(['Year', 'Month']).agg('mean')
+
+# print(df2)
+# df2.to_csv('sea_ice_extent.csv')
